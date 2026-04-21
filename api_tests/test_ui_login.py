0c4ce73e735f1b94ba57e099d300521cfcd604a1
Roman <shamilov.roman@yandex.ru>
2026-04-22 01:29:27 +0300

    Add complete step-by-step checkout test

--- api_tests/test_ui_login.py
+++ api_tests/test_ui_login.py
@@ -1,5 +1,61 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def get_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    return webdriver.Chrome(options=options)
+
+def test_open_saucedemo():
+    driver = get_driver()
+    driver.get("https://www.saucedemo.com/")
+    assert "Swag Labs" in driver.title
+    driver.quit()
+
+def test_login_valid():
+    driver = get_driver()
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    assert "inventory" in driver.current_url
+    driver.quit()
+
+def test_login_invalid():
+    driver = get_driver()
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("wrong_password")
+    driver.find_element(By.ID, "login-button").click()
+    error = driver.find_element(By.CSS_SELECTOR, "[data-test='error']")
+    assert "do not match" in error.text
+    assert "inventory" not in driver.current_url
+    driver.quit()
+
+def test_add_to_cart():
+    driver = get_driver()
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+    badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+    assert badge.text == "1"
+    driver.quit()
+
 def test_checkout_step_by_step():
-    """Тест 5a: пошаговая проверка оформления"""
+    """Тест 5: пошаговая проверка оформления заказа"""
     driver = get_driver()
     driver.get("https://www.saucedemo.com/")
     
