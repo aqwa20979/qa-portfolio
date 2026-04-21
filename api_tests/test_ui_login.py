1a9e55f54c22174b6b25287a32a9549c46768010
Roman <shamilov.roman@yandex.ru>
2026-04-22 00:11:37 +0300

    Add headless Chrome options for CI

--- api_tests/test_ui_login.py
+++ api_tests/test_ui_login.py
@@ -5,7 +5,11 @@ from selenium.webdriver.chrome.options import Options
 
 def get_driver():
     options = Options()
-    options.binary_location = "/usr/bin/chromium-browser"
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
     return webdriver.Chrome(options=options)
 
 def test_open_saucedemo():
